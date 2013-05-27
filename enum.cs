c30477814c183ceb3fc20817b5727f5a6ff506c6
vj240 <vj240@mail.ru>
2013-05-27 20:05:09 +0300

    enum commit

---
+++ enum.cs
@@ -0,0 +1,16 @@
+﻿namespace FELA.Data
+{
+
+    public enum QuestionType { Simple, RadioButton, Table, CheckBox }
+
+    public enum AnswerType { Option, Numeric, String, Table, CheckBox }
+
+    public enum Operation { Set, Add }
+
+    public enum QualifierType { EqualTo, NotEqualTo, LessThan, GreaterThan, LessThanOrEqual, GreaterThanOrEqual, Between, NotBetween }
+
+    public enum FactorType { Risk }
+
+    public enum UserMetricState { Good, AtRisk, Unknown }
+
+}
