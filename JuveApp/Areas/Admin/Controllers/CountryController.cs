bc0e8172a6f063d949531746dfc7c4afa3118e10
vj240 <vj240@mail.ru>
2013-05-29 19:46:44 +0300

    some new views and controllers

---
+++ JuveApp/Areas/Admin/Controllers/CountryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JuveApp.Areas.Admin.Controllers
+{
+    public class CountryController : Controller
+    {
+        //
+        // GET: /Admin/Country/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ JuveApp/Areas/Admin/Controllers/TeamsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Juve.Model;
+using Juve.Data;
+
+namespace JuveApp.Areas.Admin.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private JuveContext db = new JuveContext();
+
+        //
+        // GET: /Admin/Teams/
+
+        public ActionResult Index()
+        {
+            return View(db.Teams.ToList());
+        }
+
+        //
+        // GET: /Admin/Teams/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        //
+        // GET: /Admin/Teams/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Teams/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Teams.Add(team);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(team);
+        }
+
+        //
+        // GET: /Admin/Teams/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        //
+        // POST: /Admin/Teams/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Team team)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(team).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(team);
+        }
+
+        //
+        // GET: /Admin/Teams/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Team team = db.Teams.Find(id);
+            if (team == null)
+            {
+                return HttpNotFound();
+            }
+            return View(team);
+        }
+
+        //
+        // POST: /Admin/Teams/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Team team = db.Teams.Find(id);
+            db.Teams.Remove(team);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
